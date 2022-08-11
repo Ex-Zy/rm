cdc7d17ef8ecce7f0a4be039dd38f61e904c0073
Sergey Nagornykh <serega1905@ukr.net>
2022-08-11 14:27:31 +0300

    update nuxt config and npm generate script for correctly deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 import { defineNuxtConfig } from "nuxt";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: "static",
   router: {
     base: "/rm/",
+  },
+  app: {
+    baseURL: isProduction ? "/rm/" : "/",
   },
 });
