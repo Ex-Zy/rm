38acb07cda29e363a2ace7b305b8ef036cbfd30d
Sergey Nagornykh <serega1905@ukr.net>
2022-08-25 16:36:12 +0300

    update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,31 @@ import { ElContainer, ElMain, ElFooter, ElHeader } from "element-plus";
 </script>
 
 <template>
-  <ElContainer style="max-width: 1360px; padding: 0 20px; margin: 0 auto; height: 100vh">
+  <ElContainer class="app-layout">
     <ElHeader>
       <NavigationBar />
     </ElHeader>
     <ElMain>
       <slot />
     </ElMain>
-    <ElFooter style="text-align: center">
+    <ElFooter class="app-layout__footer">
       <small>&copy; Copyright {{ new Date().getFullYear() }}, Example Corporation</small>
     </ElFooter>
   </ElContainer>
 </template>
+
+<style lang="scss">
+.app-layout {
+  max-width: 1360px;
+  padding: 0 20px;
+  margin: 0 auto;
+  height: 100vh;
+  @media (max-width: 1000px) {
+    padding: 0;
+    max-width: 100%;
+  }
+  &__footer {
+    text-align: center;
+  }
+}
+</style>
