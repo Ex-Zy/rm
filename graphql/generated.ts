8c3afbe6f211b5e80692becf2a75e0be4a2720e7
Sergey Nagornykh <serega1905@ukr.net>
2022-09-01 22:57:39 +0300

    generate api schema

---
+++ graphql/generated.ts
@@ -0,0 +1,610 @@
+import gql from "graphql-tag";
+import {
+  createMutationFunction,
+  createSmartQueryOptionsFunction,
+  createSmartSubscriptionOptionsFunction,
+} from "vue-apollo-smart-ops";
+import { ApolloError } from "apollo-client";
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Upload: any;
+};
+
+export enum CacheControlScope {
+  Private = "PRIVATE",
+  Public = "PUBLIC",
+}
+
+export type Character = {
+  __typename?: "Character";
+  /** Time at which the character was created in the database. */
+  created?: Maybe<Scalars["String"]>;
+  /** Episodes in which this character appeared. */
+  episode: Array<Maybe<Episode>>;
+  /** The gender of the character ('Female', 'Male', 'Genderless' or 'unknown'). */
+  gender?: Maybe<Scalars["String"]>;
+  /** The id of the character. */
+  id?: Maybe<Scalars["ID"]>;
+  /**
+   * Link to the character's image.
+   * All images are 300x300px and most are medium shots or portraits since they are intended to be used as avatars.
+   */
+  image?: Maybe<Scalars["String"]>;
+  /** The character's last known location */
+  location?: Maybe<Location>;
+  /** The name of the character. */
+  name?: Maybe<Scalars["String"]>;
+  /** The character's origin location */
+  origin?: Maybe<Location>;
+  /** The species of the character. */
+  species?: Maybe<Scalars["String"]>;
+  /** The status of the character ('Alive', 'Dead' or 'unknown'). */
+  status?: Maybe<Scalars["String"]>;
+  /** The type or subspecies of the character. */
+  type?: Maybe<Scalars["String"]>;
+};
+
+export type Characters = {
+  __typename?: "Characters";
+  info?: Maybe<Info>;
+  results?: Maybe<Array<Maybe<Character>>>;
+};
+
+export type Episode = {
+  __typename?: "Episode";
+  /** The air date of the episode. */
+  air_date?: Maybe<Scalars["String"]>;
+  /** List of characters who have been seen in the episode. */
+  characters: Array<Maybe<Character>>;
+  /** Time at which the episode was created in the database. */
+  created?: Maybe<Scalars["String"]>;
+  /** The code of the episode. */
+  episode?: Maybe<Scalars["String"]>;
+  /** The id of the episode. */
+  id?: Maybe<Scalars["ID"]>;
+  /** The name of the episode. */
+  name?: Maybe<Scalars["String"]>;
+};
+
+export type Episodes = {
+  __typename?: "Episodes";
+  info?: Maybe<Info>;
+  results?: Maybe<Array<Maybe<Episode>>>;
+};
+
+export type FilterCharacter = {
+  gender?: InputMaybe<Scalars["String"]>;
+  name?: InputMaybe<Scalars["String"]>;
+  species?: InputMaybe<Scalars["String"]>;
+  status?: InputMaybe<Scalars["String"]>;
+  type?: InputMaybe<Scalars["String"]>;
+};
+
+export type FilterEpisode = {
+  episode?: InputMaybe<Scalars["String"]>;
+  name?: InputMaybe<Scalars["String"]>;
+};
+
+export type FilterLocation = {
+  dimension?: InputMaybe<Scalars["String"]>;
+  name?: InputMaybe<Scalars["String"]>;
+  type?: InputMaybe<Scalars["String"]>;
+};
+
+export type Info = {
+  __typename?: "Info";
+  /** The length of the response. */
+  count?: Maybe<Scalars["Int"]>;
+  /** Number of the next page (if it exists) */
+  next?: Maybe<Scalars["Int"]>;
+  /** The amount of pages. */
+  pages?: Maybe<Scalars["Int"]>;
+  /** Number of the previous page (if it exists) */
+  prev?: Maybe<Scalars["Int"]>;
+};
+
+export type Location = {
+  __typename?: "Location";
+  /** Time at which the location was created in the database. */
+  created?: Maybe<Scalars["String"]>;
+  /** The dimension in which the location is located. */
+  dimension?: Maybe<Scalars["String"]>;
+  /** The id of the location. */
+  id?: Maybe<Scalars["ID"]>;
+  /** The name of the location. */
+  name?: Maybe<Scalars["String"]>;
+  /** List of characters who have been last seen in the location. */
+  residents: Array<Maybe<Character>>;
+  /** The type of the location. */
+  type?: Maybe<Scalars["String"]>;
+};
+
+export type Locations = {
+  __typename?: "Locations";
+  info?: Maybe<Info>;
+  results?: Maybe<Array<Maybe<Location>>>;
+};
+
+export type Query = {
+  __typename?: "Query";
+  /** Get a specific character by ID */
+  character?: Maybe<Character>;
+  /** Get the list of all characters */
+  characters?: Maybe<Characters>;
+  /** Get a list of characters selected by ids */
+  charactersByIds?: Maybe<Array<Maybe<Character>>>;
+  /** Get a specific episode by ID */
+  episode?: Maybe<Episode>;
+  /** Get the list of all episodes */
+  episodes?: Maybe<Episodes>;
+  /** Get a list of episodes selected by ids */
+  episodesByIds?: Maybe<Array<Maybe<Episode>>>;
+  /** Get a specific locations by ID */
+  location?: Maybe<Location>;
+  /** Get the list of all locations */
+  locations?: Maybe<Locations>;
+  /** Get a list of locations selected by ids */
+  locationsByIds?: Maybe<Array<Maybe<Location>>>;
+};
+
+export type QueryCharacterArgs = {
+  id: Scalars["ID"];
+};
+
+export type QueryCharactersArgs = {
+  filter?: InputMaybe<FilterCharacter>;
+  page?: InputMaybe<Scalars["Int"]>;
+};
+
+export type QueryCharactersByIdsArgs = {
+  ids: Array<Scalars["ID"]>;
+};
+
+export type QueryEpisodeArgs = {
+  id: Scalars["ID"];
+};
+
+export type QueryEpisodesArgs = {
+  filter?: InputMaybe<FilterEpisode>;
+  page?: InputMaybe<Scalars["Int"]>;
+};
+
+export type QueryEpisodesByIdsArgs = {
+  ids: Array<Scalars["ID"]>;
+};
+
+export type QueryLocationArgs = {
+  id: Scalars["ID"];
+};
+
+export type QueryLocationsArgs = {
+  filter?: InputMaybe<FilterLocation>;
+  page?: InputMaybe<Scalars["Int"]>;
+};
+
+export type QueryLocationsByIdsArgs = {
+  ids: Array<Scalars["ID"]>;
+};
+
+export type PaginationFieldsFragment = {
+  __typename?: "Info";
+  count?: number | null;
+  pages?: number | null;
+  next?: number | null;
+  prev?: number | null;
+};
+
+export type LocationFieldsFragment = {
+  __typename?: "Location";
+  id?: string | null;
+  dimension?: string | null;
+  name?: string | null;
+  type?: string | null;
+  created?: string | null;
+};
+
+export type CharacterFieldsFragment = {
+  __typename?: "Character";
+  id?: string | null;
+  name?: string | null;
+  status?: string | null;
+  species?: string | null;
+  type?: string | null;
+  gender?: string | null;
+  image?: string | null;
+  created?: string | null;
+  location?: {
+    __typename?: "Location";
+    id?: string | null;
+    dimension?: string | null;
+    name?: string | null;
+    type?: string | null;
+    created?: string | null;
+  } | null;
+};
+
+export type EpisodeFieldsFragment = {
+  __typename?: "Episode";
+  id?: string | null;
+  name?: string | null;
+  air_date?: string | null;
+  episode?: string | null;
+  created?: string | null;
+  characters: Array<{
+    __typename?: "Character";
+    id?: string | null;
+    name?: string | null;
+    status?: string | null;
+    species?: string | null;
+    type?: string | null;
+    gender?: string | null;
+    image?: string | null;
+    created?: string | null;
+    location?: {
+      __typename?: "Location";
+      id?: string | null;
+      dimension?: string | null;
+      name?: string | null;
+      type?: string | null;
+      created?: string | null;
+    } | null;
+  } | null>;
+};
+
+export type GetCharactersQueryVariables = Exact<{
+  page?: InputMaybe<Scalars["Int"]>;
+  filter?: InputMaybe<FilterCharacter>;
+}>;
+
+export type GetCharactersQuery = {
+  __typename?: "Query";
+  characters?: {
+    __typename?: "Characters";
+    info?: {
+      __typename?: "Info";
+      count?: number | null;
+      pages?: number | null;
+      next?: number | null;
+      prev?: number | null;
+    } | null;
+    results?: Array<{
+      __typename?: "Character";
+      id?: string | null;
+      name?: string | null;
+      status?: string | null;
+      species?: string | null;
+      type?: string | null;
+      gender?: string | null;
+      image?: string | null;
+      created?: string | null;
+      location?: {
+        __typename?: "Location";
+        id?: string | null;
+        dimension?: string | null;
+        name?: string | null;
+        type?: string | null;
+        created?: string | null;
+      } | null;
+    } | null> | null;
+  } | null;
+};
+
+export type GetOneCharacterQueryVariables = Exact<{
+  id?: InputMaybe<Scalars["ID"]>;
+}>;
+
+export type GetOneCharacterQuery = {
+  __typename?: "Query";
+  character?: {
+    __typename?: "Character";
+    id?: string | null;
+    name?: string | null;
+    status?: string | null;
+    species?: string | null;
+    type?: string | null;
+    gender?: string | null;
+    image?: string | null;
+    created?: string | null;
+    location?: {
+      __typename?: "Location";
+      id?: string | null;
+      dimension?: string | null;
+      name?: string | null;
+      type?: string | null;
+      created?: string | null;
+    } | null;
+  } | null;
+};
+
+export type GetEpisodesQueryVariables = Exact<{
+  page?: InputMaybe<Scalars["Int"]>;
+  filter?: InputMaybe<FilterEpisode>;
+}>;
+
+export type GetEpisodesQuery = {
+  __typename?: "Query";
+  episodes?: {
+    __typename?: "Episodes";
+    info?: {
+      __typename?: "Info";
+      count?: number | null;
+      pages?: number | null;
+      next?: number | null;
+      prev?: number | null;
+    } | null;
+    results?: Array<{
+      __typename?: "Episode";
+      id?: string | null;
+      name?: string | null;
+      air_date?: string | null;
+      episode?: string | null;
+      created?: string | null;
+      characters: Array<{
+        __typename?: "Character";
+        id?: string | null;
+        name?: string | null;
+        status?: string | null;
+        species?: string | null;
+        type?: string | null;
+        gender?: string | null;
+        image?: string | null;
+        created?: string | null;
+        location?: {
+          __typename?: "Location";
+          id?: string | null;
+          dimension?: string | null;
+          name?: string | null;
+          type?: string | null;
+          created?: string | null;
+        } | null;
+      } | null>;
+    } | null> | null;
+  } | null;
+};
+
+export type GetOneEpisodesQueryVariables = Exact<{
+  id?: InputMaybe<Scalars["ID"]>;
+}>;
+
+export type GetOneEpisodesQuery = {
+  __typename?: "Query";
+  episode?: {
+    __typename?: "Episode";
+    id?: string | null;
+    name?: string | null;
+    air_date?: string | null;
+    episode?: string | null;
+    created?: string | null;
+    characters: Array<{
+      __typename?: "Character";
+      id?: string | null;
+      name?: string | null;
+      status?: string | null;
+      species?: string | null;
+      type?: string | null;
+      gender?: string | null;
+      image?: string | null;
+      created?: string | null;
+      location?: {
+        __typename?: "Location";
+        id?: string | null;
+        dimension?: string | null;
+        name?: string | null;
+        type?: string | null;
+        created?: string | null;
+      } | null;
+    } | null>;
+  } | null;
+};
+
+export const PaginationFieldsFragmentDoc = gql`
+  fragment PaginationFields on Info {
+    count
+    pages
+    next
+    prev
+  }
+`;
+export const LocationFieldsFragmentDoc = gql`
+  fragment LocationFields on Location {
+    id
+    dimension
+    name
+    type
+    created
+  }
+`;
+export const CharacterFieldsFragmentDoc = gql`
+  fragment CharacterFields on Character {
+    id
+    name
+    status
+    species
+    type
+    gender
+    image
+    created
+    location {
+      ...LocationFields
+    }
+  }
+  ${LocationFieldsFragmentDoc}
+`;
+export const EpisodeFieldsFragmentDoc = gql`
+  fragment EpisodeFields on Episode {
+    id
+    name
+    air_date
+    episode
+    created
+    characters {
+      ...CharacterFields
+    }
+  }
+  ${CharacterFieldsFragmentDoc}
+`;
+export const GetCharactersDocument = gql`
+  query GetCharacters($page: Int = 1, $filter: FilterCharacter) {
+    characters(page: $page, filter: $filter) {
+      info {
+        ...PaginationFields
+      }
+      results {
+        ...CharacterFields
+      }
+    }
+  }
+  ${PaginationFieldsFragmentDoc}
+  ${CharacterFieldsFragmentDoc}
+`;
+
+/**
+ * __useGetCharactersQuery__
+ *
+ * To use a Smart Query within a Vue component, call `useGetCharactersQuery` as the value for a query key
+ * in the component's `apollo` config, passing any options required for the query.
+ *
+ * @param options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/core/ApolloClient/#ApolloClient.query
+ *
+ * @example
+ * {
+ *   apollo: {
+ *     getCharacters: useGetCharactersQuery({
+ *       variables: {
+ *         page: // value for 'page'
+ *         filter: // value for 'filter'
+ *       },
+ *       loadingKey: 'loading',
+ *       fetchPolicy: 'no-cache',
+ *     }),
+ *   }
+ * }
+ */
+export const useGetCharactersQuery = createSmartQueryOptionsFunction<
+  GetCharactersQuery,
+  GetCharactersQueryVariables,
+  ApolloError
+>(GetCharactersDocument);
+
+export const GetOneCharacterDocument = gql`
+  query GetOneCharacter($id: ID = 1) {
+    character(id: $id) {
+      ...CharacterFields
+    }
+  }
+  ${CharacterFieldsFragmentDoc}
+`;
+
+/**
+ * __useGetOneCharacterQuery__
+ *
+ * To use a Smart Query within a Vue component, call `useGetOneCharacterQuery` as the value for a query key
+ * in the component's `apollo` config, passing any options required for the query.
+ *
+ * @param options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/core/ApolloClient/#ApolloClient.query
+ *
+ * @example
+ * {
+ *   apollo: {
+ *     getOneCharacter: useGetOneCharacterQuery({
+ *       variables: {
+ *         id: // value for 'id'
+ *       },
+ *       loadingKey: 'loading',
+ *       fetchPolicy: 'no-cache',
+ *     }),
+ *   }
+ * }
+ */
+export const useGetOneCharacterQuery = createSmartQueryOptionsFunction<
+  GetOneCharacterQuery,
+  GetOneCharacterQueryVariables,
+  ApolloError
+>(GetOneCharacterDocument);
+
+export const GetEpisodesDocument = gql`
+  query GetEpisodes($page: Int = 1, $filter: FilterEpisode) {
+    episodes(page: $page, filter: $filter) {
+      info {
+        ...PaginationFields
+      }
+      results {
+        ...EpisodeFields
+      }
+    }
+  }
+  ${PaginationFieldsFragmentDoc}
+  ${EpisodeFieldsFragmentDoc}
+`;
+
+/**
+ * __useGetEpisodesQuery__
+ *
+ * To use a Smart Query within a Vue component, call `useGetEpisodesQuery` as the value for a query key
+ * in the component's `apollo` config, passing any options required for the query.
+ *
+ * @param options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/core/ApolloClient/#ApolloClient.query
+ *
+ * @example
+ * {
+ *   apollo: {
+ *     getEpisodes: useGetEpisodesQuery({
+ *       variables: {
+ *         page: // value for 'page'
+ *         filter: // value for 'filter'
+ *       },
+ *       loadingKey: 'loading',
+ *       fetchPolicy: 'no-cache',
+ *     }),
+ *   }
+ * }
+ */
+export const useGetEpisodesQuery = createSmartQueryOptionsFunction<GetEpisodesQuery, GetEpisodesQueryVariables, ApolloError>(
+  GetEpisodesDocument,
+);
+
+export const GetOneEpisodesDocument = gql`
+  query GetOneEpisodes($id: ID = 1) {
+    episode(id: $id) {
+      ...EpisodeFields
+    }
+  }
+  ${EpisodeFieldsFragmentDoc}
+`;
+
+/**
+ * __useGetOneEpisodesQuery__
+ *
+ * To use a Smart Query within a Vue component, call `useGetOneEpisodesQuery` as the value for a query key
+ * in the component's `apollo` config, passing any options required for the query.
+ *
+ * @param options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/core/ApolloClient/#ApolloClient.query
+ *
+ * @example
+ * {
+ *   apollo: {
+ *     getOneEpisodes: useGetOneEpisodesQuery({
+ *       variables: {
+ *         id: // value for 'id'
+ *       },
+ *       loadingKey: 'loading',
+ *       fetchPolicy: 'no-cache',
+ *     }),
+ *   }
+ * }
+ */
+export const useGetOneEpisodesQuery = createSmartQueryOptionsFunction<
+  GetOneEpisodesQuery,
+  GetOneEpisodesQueryVariables,
+  ApolloError
+>(GetOneEpisodesDocument);
